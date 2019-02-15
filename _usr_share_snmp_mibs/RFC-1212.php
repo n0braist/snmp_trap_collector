938b76a57d5b12d191c6a433cc9b9a2ca98f28e6
Stefan Lange local <langes@vmd20798.contaboserver.net>
2019-02-15 10:42:15 +0100

    mibs and configs

---
+++ _usr_share_snmp_mibs/RFC-1212.php
@@ -0,0 +1,112 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>RFC-1212</title>
+<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=UTF-8">
+<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="ALL">
+<META NAME="ROBOTS" CONTENT="ALL">
+<META NAME="DESCRIPTION" CONTENT="www.circitor.fr: RFC-1212">
+<META NAME="COPYRIGHT" CONTENT="Copyright &copy; 2007 - 2013 - Circitor">
+<META NAME="KEYWORDS" CONTENT="SNMP, MIB, RFC-1212">
+<META NAME="AUTHOR" CONTENT="Circitor">
+<link rel="stylesheet" href="/Mibs/Style_mib.css" type="text/css">
+</head>
+<body>
+<div id="body_global">
+<div id="body_header">
+<img src="/Mibs/logo.png" alt="" width="100%">
+</div>
+<div id="body_main">
+<h1>RFC-1212</h1>
+File: <a href="../Files/RFC-1212.mib">RFC-1212.mib</a> (2706 bytes)
+<h2><u>Imported modules</u></h2>
+<table width="100%" border="1">
+<tr>
+<td width="33%"><a href="../Html/RFC1155-SMI.php">RFC1155-SMI</a></td>
+<td width="33%"></td>
+<td width="33%"></td>
+</table>
+<h2><u>Imported symbols</u></h2>
+<table width="100%" border="1">
+<tr>
+<td width="33%"><a href="../Html/RFC1155-SMI.php#ObjectName">ObjectName</a></td>
+<td width="33%"><a href="../Html/RFC1155-SMI.php#NetworkAddress">NetworkAddress</a></td>
+<td width="33%"><a href="../Html/RFC1155-SMI.php#IpAddress">IpAddress</a></td>
+</tr>
+</table>
+<h2><u>Defined Objects</u></h2>
+<table id=OBJECT-TYPE width="100%" border="1">
+<tr>
+<td width="25%" colspan="2" bgcolor="#c0c0c0"><ObjectName><a href="../Html/RFC-1212.php#OBJECT-TYPE">OBJECT-TYPE</a></ObjectName></td>
+<td width="25%" colspan="2" bgcolor="#c0c0c0"><ObjectOid>&nbsp;</ObjectOid></td>
+</tr>
+<tr>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>MACRO</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType></ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+</tr>
+</table>
+<br>
+<table id=IndexSyntax width="100%" border="1">
+<tr>
+<td width="25%" colspan="2" bgcolor="#c0c0c0"><ObjectName><a href="../Html/RFC-1212.php#IndexSyntax">IndexSyntax</a></ObjectName></td>
+<td width="25%" colspan="2" bgcolor="#c0c0c0"><ObjectOid>&nbsp;</ObjectOid></td>
+</tr>
+<tr>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>CHOICE</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType></ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+</tr>
+<tr>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>INTEGER as number</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>0..MAX</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+</tr>
+<tr>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>OCTET STRING as string</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType></ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+</tr>
+<tr>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>OBJECT IDENTIFIER as object</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType></ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+</tr>
+<tr>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType><a href="../Html/RFC1155-SMI.php#NetworkAddress">NetworkAddress</a> as address</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType></ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+</tr>
+<tr>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType><a href="../Html/RFC1155-SMI.php#IpAddress">IpAddress</a> as ipAddress</ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType></ObjectType></td>
+<td width="25%" colspan="1" bgcolor="#ffffff"><ObjectType>&nbsp;</ObjectType></td>
+</tr>
+</table>
+<br>
+</div>
+<div id="body_footer">
+Generation date: June 4th, 2013 17:50:00, <a href="http://www.circitor.fr">Copyright &copy; 2007 - 2013 - Circitor</a>
+</div>
+</div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+<br />
+<b>Warning</b>:  printf() [<a href='function.printf'>function.printf</a>]: Too few arguments in <b>/home/circitor/www/Mibs/Function.php</b> on line <b>106</b><br />
+</script>
+<script type="text/javascript">
+try{
+var pageTracker = _gat._getTracker("UA-26754465-1");
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
+</body>
+</html>
